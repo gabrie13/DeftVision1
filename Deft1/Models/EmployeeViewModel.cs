83590e5342e4ee5b0588d89cbe27cd8cbae03337
Gabriel Barrett <gbarrett2@gmail.com>
2017-09-13 23:09:18 -0700

    created EmployeePosition, EmployeePositionViewModel, Employee, EmployeeViewModel

---
+++ Deft1/Models/EmployeeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Deft1.Models
+{
+    public class EmployeeViewModel
+    {
+    }
+}
